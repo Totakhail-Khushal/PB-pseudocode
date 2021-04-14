09a909f820cb92f608ceb54a56de2e5d2a37db60
khushal <totakhailkhushal@gmail.com>
2021-04-14 11:21:10 +0200

    pseudo done

---
+++ .history/start_20210414111030.js
@@ -0,0 +1,52 @@
+// # Inputs and Outputs
+
+// Write down pseudo - code on pen and paper for the following patterns:
+
+//     1.
+// Input => Output
+// 2, 5	  => 5
+// 0, 100	=> 100
+// 52, 35	=> 52
+
+if (2 < 5) {
+    console.log(true, 5)
+} else {
+    console.log(false)
+}
+
+if (0 > 100) {
+    console.log("zero is greater")
+} else {
+    console.log("smaller")
+}
+
+if (52 > 35) {
+    console.log(true, 5)
+} else {
+    console.log(false)
+}
+
+
+// 2.
+// Input => Output
+// 5, 5     => true
+
+if (5 > 5) {
+    console.log(true)
+} else {
+    console.log(true)
+}
+
+// 40, 40   => true
+if (40 > 40) {
+    console.log(true)
+} else {
+    console.log(true)
+}
+// 50, 51   => false
+
+// 3.
+// Input => Output
+// 110, 100	 => true
+// 150, 100	 => true
+// 50, 100	   => false
---
+++ .history/start_20210414111301.js
@@ -0,0 +1,72 @@
+// # Inputs and Outputs
+
+// Write down pseudo - code on pen and paper for the following patterns:
+
+//     1.
+// Input => Output
+// 2, 5	  => 5
+// 0, 100	=> 100
+// 52, 35	=> 52
+
+if (2 < 5) {
+    console.log(true, 5)
+} else {
+    console.log(false)
+}
+
+if (0 > 100) {
+    console.log("zero is greater")
+} else {
+    console.log("smaller")
+}
+
+if (52 > 35) {
+    console.log(true, 5)
+} else {
+    console.log(false)
+}
+
+
+// 2.
+// Input => Output
+// 5, 5     => true
+
+if (5 > 5) {
+    console.log(true)
+} else {
+    console.log(true)
+}
+
+// 40, 40   => true
+if (40 > 40) {
+    console.log(true)
+} else {
+    console.log(true)
+}
+// 50, 51   => false
+if (50 > 51) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+// 3.
+// Input => Output
+// 110, 100	 => true
+if (110 > 100) {
+    console.log(true)
+} else {
+    console.log(true)
+}
+// 150, 100	 => true
+if (150 > 100) {
+    console.log(true)
+} else {
+    console.log(true)
+}
+// 50, 100	   => false
+if (50 > 100) {
+    console.log(true)
+} else {
+    console.log(true)
+}
